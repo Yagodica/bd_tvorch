3d01040a2156956e052d4850549a3773c2dd3b7b
Yagodica <ivantey228@gmail.com>
2024-12-02 12:29:25 +0300

    cal peredelati

--- insert_delivery.sql
+++ insert_delivery.sql
@@ -46,7 +46,60 @@ INSERT INTO client (client_id, client_name, client_surname, phone_num, client_em
 (6, 'Charl', 'Davis', '7828282882', 'charl.davis@example.com'),
 (7, 'Arlie', 'Davis', '1234123412', 'arlie.davis@example.com'),
 (8, 'Lie', 'Davis', '8238382188', 'lie.davis@example.com'),
-(9, 'Eve', 'Wilson', '6786712389', 'eve.wilson@example.com');
+(9, 'Eve', 'Wilson', '6786712389', 'eve.wilson@example.com'),
+(10, 'Ivan', 'Wilson', '617862312389', 'ivan.wilson@example.com');
+
+-- Вставка данных в таблицу address частный дом
+INSERT INTO address (address_id, house_id_house, client_client_id) VALUES
+(1, 5, 1),
+(2, 6, 2),
+(3, 7, 3),
+(4, 8, 4),
+(5, 9, 5);
+
+-- Вставка данных в таблицу address квартира
+INSERT INTO address (address_id, apartment_apartment_id, client_client_id) VALUES
+(6, 1, 6),
+(7, 2, 7),
+(8, 3, 8),
+(9, 4, 9),
+(10, 5, 10);
+
+-- Вставка данных в таблицу order_status
+INSERT INTO order_status (order_status_id, order_status_name) VALUES
+(1, 'В ожидании'),
+(2, 'Обработка'),
+(3, 'Отправленный'),
+(4, 'Доставленный'),
+(5, 'Отменено');
+
+-- Вставка данных в таблицу orders
+INSERT INTO orders (order_id, order_date, address_address_id, order_status_order_status_id, client_client_id, cart_cart_id) VALUES
+(1, CURDATE(), 1, 1, 1, 1),
+(2, CURDATE(), 2, 3, 2, 2),
+(3, CURDATE(), 3, 3, 3, 3),
+(4, CURDATE(), 4, 3, 4, 4),
+(5, CURDATE(), 5, 3, 5, 5),
+(6, CURDATE(), 6, 3, 6, 6),
+(7, CURDATE(), 7, 4, 7, 7),
+(8, CURDATE(), 8, 4, 8, 8),
+(9, CURDATE(), 9, 5, 9, 9),
+(10, CURDATE(), 10, 2, 10, 10);
+
+
+-- Вставка данных в таблицу cart
+INSERT INTO cart (cart_id, quantity, total_cost, orders_order_id) VALUES
+(1, 2, 2000, 1),
+(2, 1, 1000, 2),
+(3, 3, 3000, 3),
+(4, 2, 2000, 4),
+(5, 1, 1000, 5),
+(6, 2, 2000, 1),
+(7, 1, 1000, 2),
+(8, 3, 3000, 3),
+(9, 2, 2000, 4),
+(10, 1, 1000, 5);
+
 
 -- Вставка данных в таблицу category
 INSERT INTO category (category_id, category_name) VALUES
@@ -75,32 +128,11 @@ INSERT INTO dish (dish_id, dish_name, dish_description, dish_price, category_cat
 (5, 'Chocolate Cake', 'Delicious chocolate cake', 600, 4, 5),
 (6, 'Coca Cola', 'Soft drink', 200, 5, 6);
 
--- Вставка данных в таблицу order_status
-INSERT INTO order_status (order_status_id, order_status_name) VALUES
-(1, 'Pending'),
-(2, 'Processing'),
-(3, 'Shipped'),
-(4, 'Delivered'),
-(5, 'Cancelled'),
-(6, 'Returned');
 
--- Вставка данных в таблицу orders
-INSERT INTO orders (order_id, order_date, address_address_id, order_status_order_status_id, client_client_id, cart_cart_id) VALUES
-(1, '2023-10-01', 1, 1, 1, 1),
-(2, '2023-10-02', 2, 2, 2, 2),
-(3, '2023-10-03', 3, 3, 3, 3),
-(4, '2023-10-04', 4, 4, 4, 4),
-(5, '2023-10-05', 5, 5, 5, 5),
-(6, '2023-10-06', 6, 6, 6, 6);
 
--- Вставка данных в таблицу cart
-INSERT INTO cart (cart_id, quantity, total_cost, orders_order_id) VALUES
-(1, 2, 2000, 1),
-(2, 1, 1000, 2),
-(3, 3, 3000, 3),
-(4, 2, 2000, 4),
-(5, 1, 1000, 5),
-(6, 3, 3000, 6);
+
+
+
 
 -- Вставка данных в таблицу dish_cart
 INSERT INTO dish_cart (cart_cart_id, dish_dish_id) VALUES
